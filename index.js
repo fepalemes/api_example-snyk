f4e025189481e501eb8dab471f21329f588b6361
Felipe Lemes <fepalemes@gmail.com>
2023-04-08 17:07:27 -0300

    Commit to Snyk monitor

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const portExec = process.env.PORT;
 app.use(express.json());
 app.use(routes);
 
+// test code
+let user = "user-test";
+let pass = "123@test";
+
+console.info(`My user is ${user} and my pass is ${pass}`);
+
 app.listen(portExec, () =>
   console.log('========== APPLICATION STARTED ==========')
 );
